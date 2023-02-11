4a60cfcc9654f00e8ba6e674eb0632b1178b3544
*Vasisualey* <basilnekrasov@mail.ru>
2023-02-11 20:03:04 +0300

    Решил задачу 24

---
+++ Task24/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число А и 
+//выдает сумму чисел от 1 до А.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!)!;
+Console.WriteLine($"Сумма цифр = {GetSumNums(num)}");
+
+int GetSumNums(int number)
+{
+    int sum =0;
+    while(number>0)
+    {
+        sum+=number;
+        Console.Write(sum+ " ");
+        number--; 
+    }
+    return sum;
+}
