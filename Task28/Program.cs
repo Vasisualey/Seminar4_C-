0ea48b1bd6c7d37dd39a978954c7c1d1478ff9d7
*Vasisualey* <basilnekrasov@mail.ru>
2023-02-12 14:03:44 +0300

    Решил задачу 28

---
+++ Task28/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдает произведение чисел от 1 до N.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!)!;
+
+Console.WriteLine($"Произведение чисел: {getMultiply(number)}");
+int getMultiply(int number)
+{
+    int temp = 1;
+    for(int count = 1; count<=number; count++)
+    {
+        temp = temp*count;
+    }
+return temp;
+}
+
