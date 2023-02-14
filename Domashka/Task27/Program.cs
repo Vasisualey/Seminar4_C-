7e15311ff059d18f7a1013a31311c43d62be4ebc
Vasisualey <basilnekrasov@mail.ru>
2023-02-14 20:26:11 +0300

    решил 27 задачу

---
+++ Domashka/Task27/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Clear();
+int Insert(string message)
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine()!)!;
+    return result;
+}
+
+int Sum(int number)
+{
+    int result =0;
+    while (number > 0)
+    {
+        result = result + number%10;
+        number = number/10;
+    }
+    return result;
+}
+
+int number = Insert("Введите число: ");
+Console.WriteLine($"Сумма цифр в числе {number} равна {Sum(number)}.");
+
+
