9776a7c2208633c4390bc4db8f6f2df78580e31b
Vasisualey <basilnekrasov@mail.ru>
2023-02-14 18:55:52 +0300

    решил 25 задачу

---
+++ Domashka/Task25/Program.cs
@@ -0,0 +1,40 @@
+﻿// SЗадача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.Clear();
+int Insert(string message)
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine()!)!;
+    return result;
+}
+
+int Process(int Base, int Valent)
+{
+    int Process = 1;
+    for (int i = 0; i < Valent;i++)
+    {
+        Process = Process*Base;
+    }
+    return Process;
+}
+
+bool Trueorfalse(int Valent)
+{
+    if (Valent < 0)
+    {
+        Console.WriteLine("Степень не может быть меньше нуля");
+        return false;
+    }
+    return true;
+}
+
+int Base = Insert("Введите возводимое в степень число: ");
+int Valent = Insert("Введите степень числа: ");
+if(Trueorfalse(Valent))
+{
+    Console.WriteLine($"Число {Base} в степени {Valent} равно {Process(Base, Valent)}.");
+}
